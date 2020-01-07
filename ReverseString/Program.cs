cb26bb50b2b08bffaa49470910d0bcd9a1e10cfa
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-01-06 23:19:33 -0800

    Added ReverseString exercise

---
+++ ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString
+{
+    // --- Directions
+    // Given a string, return a new string with the reversed
+    // order of characters
+    // --- Examples
+    //   reverse('apple') === 'leppa'
+    //   reverse('hello') === 'olleh'
+    //   reverse('Greetings!') === '!sgniteerG'
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "Greetings!";    
+            Console.WriteLine(ReverseStr(str));
+        }
+        public static string ReverseStr(string str)
+        {
+            return new string(str.Reverse().ToArray());
+        }
+    }
+}
