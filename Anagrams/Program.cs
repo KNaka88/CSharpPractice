8deb4679b8e129be701928181a097fd51aebdf86
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-01-10 22:36:40 -0800

    Added Anagrams exercise

---
+++ Anagrams/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Anagrams
+{
+    // --- Directions
+    // Check to see if two provided strings are anagrams of eachother.
+    // One string is an anagram of another if it uses the same characters
+    // in the same quantity. Only consider characters, not spaces
+    // or punctuation.  Consider capital letters to be the same as lower case
+    // --- Examples
+    //   anagrams('rail safety', 'fairy tales') --> True
+    //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+    //   anagrams('Hi there', 'Bye there') --> False
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CleanUp("RAIL! SAFETY!") == CleanUp("fairy tales"));
+        }
+
+        static string CleanUp(string str)
+        {
+            var replacedStr = Regex.Replace(str.ToLower(), @"\W", "");
+            return new string(replacedStr.OrderBy(x => x).ToArray());
+        }
+    }
+}
