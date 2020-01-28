c3d387cd850bf2a897477492e9c7456b24ff5bbe
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-01-27 21:34:27 -0800

    Added Steps exercise

---
+++ Steps/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Steps
+{
+    // --- Directions
+    // Write a function that accepts a positive number N.
+    // The function should console log a step shape
+    // with N levels using the # character.  Make sure the
+    // step has spaces on the right hand side!
+    // --- Examples
+    //   steps(2)
+    //       '# '
+    //       '##'
+    //   steps(3)
+    //       '#  '
+    //       '## '
+    //       '###'
+    //   steps(4)
+    //       '#   '
+    //       '##  '
+    //       '### '
+    //       '####'
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 5;
+
+            Steps(n);
+        }
+
+        static void Steps(int n)
+        {
+            for (var i = 1; i <= n; i++)
+            {
+                var steps = string.Concat(Enumerable.Repeat("#", i)) + string.Concat(Enumerable.Repeat(" ", n - i));
+                Console.WriteLine(steps);
+            }
+        }
+    }
+}
