5a6227e4a6511f64a6f5a50265e8345d885328ef
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-01-27 22:41:54 -0800

    Added Pyramid exercise

---
+++ Pyramid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 10;
+            Pyramid(n);
+        }
+
+        static void Pyramid(int n)
+        {
+            var neededLength = n * 2 - 1;
+
+            for (var i = 1; i <= n; i++)
+            {
+                var hash = string.Concat(Enumerable.Repeat("#", i * 2 - 1));
+                var space = string.Concat(Enumerable.Repeat(" ", neededLength - (i * 2 - 1) / 2));
+                Console.WriteLine(space + hash + space);               
+            }
+        }
+    }
+}
