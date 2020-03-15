8cdcffbfdcbfdf379c29e395254295099f1ab6ae
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-03-14 22:48:02 -0700

    Added Vowels

---
+++ Vowels/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Vowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "This is not my IDEA";
+            var rx = new Regex(@"[aeiou]", RegexOptions.IgnoreCase);
+            Console.WriteLine(rx.Matches(str).Count);
+        }
+    }
+}
